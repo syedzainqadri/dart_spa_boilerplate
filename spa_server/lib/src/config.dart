455c53887844ade6b6af18739cf25114c3a0cbb5
EwigLife <96788793+syedzainqadri@users.noreply.github.com>
2023-01-27 20:32:43 +0500

    Update config.dart

--- spa_server/lib/src/config.dart
+++ spa_server/lib/src/config.dart
@@ -1,13 +1,13 @@
-import 'package:envify/envify.dart';
+// import 'package:envify/envify.dart';
 
-part 'config.g.dart';
+// part 'config.g.dart';
 
-@Envify()
-abstract class Env {
-  static const String secretKey = _Env.secretKey;
-  static const String mongoUrl = _Env.mongoUrl;
-  static const String redisHost = _Env.redisHost;
-  static const String redisPassword = _Env.redisPassword;
-  static const int redisPort = _Env.redisPort;
-  static const int serverPort = _Env.serverPort;
-}
+// @Envify()
+// abstract class Env {
+//   static const String secretKey = _Env.secretKey;
+//   static const String mongoUrl = _Env.mongoUrl;
+//   static const String redisHost = _Env.redisHost;
+//   static const String redisPassword = _Env.redisPassword;
+//   static const int redisPort = _Env.redisPort;
+//   static const int serverPort = _Env.serverPort;
+// }
